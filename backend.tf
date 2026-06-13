f669b4c779e5012a3d5805433305563e39b20e72
damnYura <yurii.boiko.kb.2023@lpnu.ua>
2026-06-13 14:34:37 +0300

    fix: update S3 bucket account ID to 522868276919

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "voting-app-terraform-state-179686849870"
+    bucket         = "voting-app-terraform-state-522868276919"
     key            = "eks/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-locks"
