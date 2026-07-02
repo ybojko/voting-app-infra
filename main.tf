e613a505d8df5d2d191b27a6f8af24776490b649
damnYura <yurii.boiko.kb.2023@lpnu.ua>
2026-07-02 20:11:20 +0300

    chore: switch VPC CIDR to 10.1.0.0/16 (avoid orphaned VPC conflict)

--- main.tf
+++ main.tf
@@ -35,11 +35,11 @@ module "vpc" {
   version = "~> 5.0"
 
   name = "${var.cluster_name}-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
