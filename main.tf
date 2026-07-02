ec523d71edafd292ed40dc653e31fa8001f082b4
damnYura <yurii.boiko.kb.2023@lpnu.ua>
2026-07-02 16:12:57 +0300

    fix: cluster_certificate_authority_data attribute name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.certificate_authority_data)
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
@@ -21,7 +21,7 @@ provider "helm" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
